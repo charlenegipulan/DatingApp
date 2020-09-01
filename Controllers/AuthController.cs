d63777b8557279dbba3c112940f16a6ac1799abf
Charlene Gipulan <charlenegipulan@gmail.com>
2020-08-31 21:59:58 -0700

    setup global exception handler

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -47,8 +47,6 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            try 
-            {
                 throw new Exception("Computer says no");
 
                 var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
@@ -81,11 +79,7 @@ namespace DatingApp.API.Controllers
                 return Ok(new
                 {
                     token = tokenHandler.WriteToken(token)
-                });
-
-            } catch {
-                return StatusCode(500, "Computer really says no!");
-            }
+                });            
             
         }
     }
