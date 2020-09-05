2b19049b40fbe3794822fc18236d7eed4bdf4fca
Charlene Gipulan <charlenegipulan@gmail.com>
2020-09-04 22:51:06 -0700

    seeded data into database

---
+++ Data/Seed.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Models;
+using Newtonsoft.Json;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        public static void SeedUsers(DataContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                //we get a list of users that we can loop through and do something with here
+                foreach (var user in users)
+                {
+                    byte[] passwordhash, passwordSalt;
+                    CreatePasswordHash("password", out passwordhash, out passwordSalt);
+
+                    user.PasswordHash = passwordhash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
+                    context.Users.Add(user);
+                }
+                // after we populate all user object with the properties it needs,
+                //and we added it to our context
+                //we need to save it
+
+                context.SaveChanges();
+
+            }
+        }
+
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
