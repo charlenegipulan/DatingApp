868a4f1cfde8b06e625cf9775797ca09be951797
Charlene Gipulan <charlenegipulan@gmail.com>
2020-09-06 17:24:47 -0700

    create Dating repository interface

---
+++ Data/IDatingRepository.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IDatingRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+
+        //get all users
+        Task<IEnumerable<User>> GetUsers();
+
+        //get individual users from database
+        Task<User> GetUser(int id);
+    }
+}
